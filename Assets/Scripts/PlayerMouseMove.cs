73a32d54bffd8987a94ca2c89a604880b6bb3013
maxiS1X <159305731+maxiS1X@users.noreply.github.com>
2024-04-19 17:55:05 +0300

    fps script fix

---
+++ Assets/Scripts/PlayerMouseMove.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMouseMove : MonoBehaviour
+{
+    float xRot;
+    float yRot;
+    float xRotCurrent;
+    float yRotCurrent;
+    public Camera playerCamera;
+    public GameObject playerGameObject;
+    public float sensivity = 5f;
+    public float smoothTime = 0.1f;
+    float currentVelocityX;
+    float currentVelocityY;
+
+    private void Update()
+    {
+        MouseMove();
+    }
+    private void MouseMove()
+    {
+        xRot += Input.GetAxis("Mouse X") * sensivity;
+        yRot += Input.GetAxis("Mouse Y") * sensivity;
+        yRot = Mathf.Clamp(yRot,-45, 45);
+
+        xRotCurrent = Mathf.SmoothDamp(xRotCurrent, xRot, ref currentVelocityX, smoothTime);
+        yRotCurrent = Mathf.SmoothDamp(yRotCurrent, yRot, ref currentVelocityY, smoothTime);
+
+        playerCamera.transform.rotation = Quaternion.Euler(-yRotCurrent, xRotCurrent, 0f);
+        playerGameObject.transform.rotation = Quaternion.Euler(0f, xRotCurrent, 0f);
+    }
+}
