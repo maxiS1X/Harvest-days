57e0876e83294a7c72282dfc4a6fe72d310a6755
FazerSemiDark <146717795+FazerSemiDark@users.noreply.github.com>
2024-05-14 22:38:37 +0500

    Trade System 2
    Уже можно получать деньги

--- Assets/Scripts/Player/TraderSearch.cs
+++ Assets/Scripts/Player/TraderSearch.cs
@@ -1,12 +1,8 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TradeSearch : MonoBehaviour
 {
     [SerializeField] private float reachDistance;
-    [SerializeField] private Transform _tradePanel;
-    [SerializeField] private Money _money;
-    public List<InventorySlot> slots = new List<InventorySlot>();
     private Camera _mainCamera;
 
     void Start()
@@ -17,18 +13,8 @@ public class TradeSearch : MonoBehaviour
     {
         _mainCamera = Camera.main;
     }
-    private void ListFilling()
-    {
-        // Заполнение списка
-        for (int i = 0; i < _tradePanel.childCount; i++)
-        {
-            if (_tradePanel.GetChild(i).GetComponent<InventorySlot>() != null)
-            {
-                slots.Add(_tradePanel.GetChild(i).GetComponent<InventorySlot>());
-            }
-        }
-    }
-    void Update()
+
+    private void Update()
     {
         Search();
     }
@@ -37,46 +23,13 @@ public class TradeSearch : MonoBehaviour
         Ray ray = new Ray(_mainCamera.transform.position, _mainCamera.transform.forward); // Пускаем луч из камеры
         RaycastHit hit;
 
-        if (Physics.Raycast(ray, out hit, reachDistance))
+        if (Input.GetMouseButtonDown(1)) // 
         {
-            if (Input.GetMouseButton(1) && hit.collider.GetComponent<Trader>() != null)
+            if (Physics.Raycast(ray, out hit, reachDistance) && hit.collider.GetComponent<Trader>() != null)
             {
-                DetermineID(hit.collider.GetComponent<Trader>()._tradersID);
+                Debug.Log("!null");
+                hit.collider.GetComponent<Trader>().DetermineID(hit.collider.GetComponent<Trader>()._tradersID);
             }
         }
-    }
-    public void DetermineID(int _tradersID)
-    {
-        switch (_tradersID)
-        {
-            case 0:
-                SellTrader();
-                break;
-
-            case 1:
-                BuyTrader();
-                break;
-        }
-    }
-    private void SellTrader()
-    {
-        int profit = 0;
-        foreach (InventorySlot slot in slots)
-        {
-            if (slot.item == null)
-            {
-                if (slot.item.canSell == true)
-                {
-                    profit += slot.amount * slot.item.price;
-                    return;
-                }
-                continue;
-            }
-            _money.MoneyValue += profit;
-        } 
-    }
-    private void BuyTrader()
-    {
-
     }
 }
--- Assets/Scripts/Trade/Trader.cs
+++ Assets/Scripts/Trade/Trader.cs
@@ -1,6 +1,61 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Trader : MonoBehaviour
 {
     public int _tradersID;
+    [SerializeField] private Transform _tradeSlots;
+    [SerializeField] private GameObject _tradePanel;
+    [SerializeField] private Money _money;
+    public List<InventorySlot> slots = new List<InventorySlot>();
+
+    void Start()
+    {
+        ListFilling();
+    }
+    private void ListFilling()
+    {
+        // Заполнение списка
+        for (int i = 0; i < _tradeSlots.transform.childCount; i++)
+        {
+            if (_tradeSlots.transform.GetChild(i).GetComponent<InventorySlot>() != null)
+            {
+                slots.Add(_tradeSlots.transform.GetChild(i).GetComponent<InventorySlot>());
+            }
+        }
+    }
+    public void DetermineID(int _tradersID)
+    {
+        switch (_tradersID)
+        {
+            case 0:
+                _tradePanel.SetActive(true);
+                break;
+
+            case 1:
+                BuyTrader();
+                break;
+        }
+    }
+    public void SellTrader()
+    {
+        int profit = 0;
+        foreach (InventorySlot slot in slots)
+        {
+            if (slot.item != null)
+            {
+                if (slot.item.canSell == true)
+                {
+                    profit += slot.amount * slot.item.price;
+                    Debug.Log(profit);
+                }
+            }
+            _money.MoneyValue += profit;
+            profit = 0;
+        }
+    }
+    private void BuyTrader()
+    {
+
+    }
 }
