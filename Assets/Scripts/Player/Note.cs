08370dbc0531afac2c386f868c14f7597a1b0bdb
maxiS1X <159305731+maxiS1X@users.noreply.github.com>
2024-05-23 19:29:54 +0300

    Mail

--- Assets/Scripts/Player/Note.cs
+++ Assets/Scripts/Player/Note.cs
@@ -1,29 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.SceneManagement;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
+using TMPro;
 
 public class Note : MonoBehaviour
 {
     public string noteTextstr;
     public GameObject notice;
     public GameObject noteUI;
-    public Text text;
+    public TMP_Text text;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     private void OnTriggerStay(Collider other)
     {
-        text.text = noteTextstr;
+       // text.text = noteTextstr;
         if (Input.GetKey(KeyCode.E))
         {
             noteUI.SetActive(true);
